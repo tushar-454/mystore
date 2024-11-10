744ead5f2c62fd5676228c1d89700c7792ae3c12
tushar-454 <imtushar454@gmail.com>
2024-11-11 00:35:13 +0600

    storeit project init

---
+++ packages/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className='text-error text-3xl'>
+        StoreIt - The only storage solution you'll ever need.
+      </h1>
+    </main>
+  );
+}
