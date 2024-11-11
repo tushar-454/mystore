445d299d264d26745b4a0aee84bf6ff990faf9bd
tushar-454 <imtushar454@gmail.com>
2024-11-11 22:39:17 +0600

    remove monorepo setup

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from '@/components/AuthForm';
+
+const SignUp = () => {
+  return <AuthForm type='sign-up' />;
+};
+
+export default SignUp;
--- packages/frontend/app/(auth)/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-const SignUp = () => {
-  return (
-    <div>
-      <h1>this is the sign up page</h1>
-    </div>
-  );
-};
-
-export default SignUp;
