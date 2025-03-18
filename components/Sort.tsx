c140f8a5410c778017f516a909815fd37c9ca506
tushar-454 <imtushar454@gmail.com>
2025-03-18 16:40:22 +0600

    implement sorting functionality in Sort component; add custom Select component for enhanced user experience

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,5 +1,37 @@
+'use client';
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import sortTypes from '@/constants/sortTypes';
+import { usePathname, useRouter } from 'next/navigation';
+
 const Sort = () => {
-  return <div>Sort</div>;
+  const router = useRouter();
+  const path = usePathname();
+
+  const handleSortChange = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSortChange} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className='sort-select'>
+        <SelectValue placeholder={sortTypes[0].label} />
+      </SelectTrigger>
+      <SelectContent className='sort-select-content'>
+        {sortTypes.map((sortType) => (
+          <SelectItem key={sortType.value} value={sortType.value}>
+            {sortType.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default Sort;
