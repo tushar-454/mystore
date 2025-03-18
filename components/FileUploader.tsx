8c6156483ba9d1b6e688b51b3bb774924df5de7b
tushar-454 <imtushar454@gmail.com>
2025-03-18 16:06:51 +0600

    implement file uploader and search components; enhance file upload handling and add search functionality

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,60 +1,64 @@
 'use client';
-
+import Thumbnail from '@/components/Thumbnail';
 import { Button } from '@/components/ui/button';
 import { MAX_FILE_SIZE } from '@/constants/others';
 import { useToast } from '@/hooks/use-toast';
 import { uploadFile } from '@/lib/actions/file.action';
 import { cn, convertFileToUrl, getFileType } from '@/lib/utils';
-import images from '@/public';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
-import { useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import Thumbnail from './Thumbnail';
 
-interface FileUploaderProps {
+interface Props {
   ownerId: string;
   accountId: string;
   className?: string;
 }
 
-const FileUploader = ({ ownerId, accountId, className }: FileUploaderProps) => {
-  const [files, setFiles] = useState<File[]>([]);
-  const { toast } = useToast();
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
   const path = usePathname();
+  const { toast } = useToast();
+  const [files, setFiles] = useState<File[]>([]);
 
   const onDrop = useCallback(
     async (acceptedFiles: File[]) => {
       setFiles(acceptedFiles);
-      const uploadPromise = acceptedFiles.map((file) => {
+
+      const uploadPromises = acceptedFiles.map(async (file) => {
         if (file.size > MAX_FILE_SIZE) {
           setFiles((prevFiles) =>
-            prevFiles.filter((prevFile) => prevFile.name !== file.name),
+            prevFiles.filter((f) => f.name !== file.name),
           );
+
           return toast({
             description: (
               <p className='body-2 text-white'>
-                <span className='font-semibold'>{file.name} </span>
-                is too large. Max file size is 50MB.
+                <span className='font-semibold'>{file.name}</span> is too large.
+                Max file size is 50MB.
               </p>
             ),
             className: 'error-toast',
           });
         }
+
         return uploadFile({ file, ownerId, accountId, path }).then(
           (uploadedFile) => {
-            setFiles((prevFiles) =>
-              prevFiles.filter((f) => f.name !== file.name),
-            );
+            if (uploadedFile) {
+              setFiles((prevFiles) =>
+                prevFiles.filter((f) => f.name !== file.name),
+              );
+            }
           },
         );
       });
-      await Promise.all(uploadPromise);
+
+      await Promise.all(uploadPromises);
     },
     [ownerId, accountId, path],
   );
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   const handleRemoveFile = (
     e: React.MouseEvent<HTMLImageElement, MouseEvent>,
@@ -65,17 +69,24 @@ const FileUploader = ({ ownerId, accountId, className }: FileUploaderProps) => {
   };
 
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className='cursor-pointer'>
       <input {...getInputProps()} />
       <Button type='button' className={cn('uploader-button', className)}>
-        <Image src={images.upload} alt='upload' width={24} height={24} />
-        <p>Uplaod</p>
+        <Image
+          src='/assets/icons/upload.svg'
+          alt='upload'
+          width={24}
+          height={24}
+        />{' '}
+        <p>Upload</p>
       </Button>
       {files.length > 0 && (
         <ul className='uploader-preview-list'>
           <h4 className='h4 text-light-100'>Uploading</h4>
+
           {files.map((file, index) => {
             const { type, extension } = getFileType(file.name);
+
             return (
               <li
                 key={`${file.name}-${index}`}
@@ -87,22 +98,23 @@ const FileUploader = ({ ownerId, accountId, className }: FileUploaderProps) => {
                     extension={extension}
                     url={convertFileToUrl(file)}
                   />
+
                   <div className='preview-item-name'>
                     {file.name}
                     <Image
-                      src={images.fileLoader}
-                      alt='file loader'
+                      src='/assets/icons/file-loader.gif'
                       width={80}
                       height={26}
+                      alt='Loader'
                     />
                   </div>
                 </div>
+
                 <Image
-                  src={images.remove}
-                  alt='remove icon'
+                  src='/assets/icons/remove.svg'
                   width={24}
                   height={24}
-                  className='cursor-pointer'
+                  alt='Remove'
                   onClick={(e) => handleRemoveFile(e, file.name)}
                 />
               </li>
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,99 @@
+'use client';
+import { getFiles } from '@/lib/actions/file.action';
+import Image from 'next/image';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { Models } from 'node-appwrite';
+import { useEffect, useState } from 'react';
+import FormattedDateTime from './FormattedDateTime';
+import Thumbnail from './Thumbnail';
+import { Input } from './ui/input';
+
 const Search = () => {
-  return <div>Search component</div>;
+  const [query, setQuery] = useState('');
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get('query') || '';
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+
+  const handleClickItem = (file: Models.Document) => {
+    setOpen(false);
+    setResults([]);
+
+    router.push(
+      `/${file.type === 'video' || file.type === 'audio' ? 'media' : file.type + 's'}?query=${query}`,
+    );
+  };
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      if (!query) {
+        setResults([]);
+        setOpen(false);
+        router.push(path.replace(searchParams.toString(), ''));
+      }
+      const files = await getFiles({ searchText: query });
+      setResults(files.documents);
+      setOpen(true);
+    };
+    fetchFiles();
+  }, [query]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery('');
+    }
+  }, [searchQuery]);
+  return (
+    <div className='search'>
+      <div className='search-input-wrapper'>
+        <Image
+          src={'/assets/icons/search.svg'}
+          alt='search'
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder='search ...'
+          className='search-input'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        {open && (
+          <ul className='search-result'>
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  key={file.$id}
+                  className='flex items-center justify-between'
+                  onClick={() => handleClickItem(file)}
+                >
+                  <div className='flex cursor-pointer items-center gap-4'>
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className='size-9 min-w-9'
+                    />
+                    <p className='subtitle-2 line-clamp-1 text-light-100'>
+                      {file.name}
+                    </p>
+                  </div>
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className='caption line-clamp-1 text-light-200'
+                  />
+                </li>
+              ))
+            ) : (
+              <p className='empty-result'>No files found</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
